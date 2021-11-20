ad44248b4ebf0a0db3b24d10357234620cf92f5e
Bucora <lubos.martincek2@gmail.com>
2021-11-20 18:58:56 +0100

    placeholder listy v user profile

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
 export const User = ({user}) => {
+
+    const numbers = [1, 2, 3, 4, 5];
+    const listItems = numbers.map((number) =>
+    <li>{number}</li>
+    );
+
     return (
         <div>
             {Object.keys(user).length !== 0 &&
-                <div>logged in: {user}</div>
+                <p>logged in: {user}</p>
+            }
+            {Object.keys(user).length !== 0 && <div>
+                <p>Moje konferencie</p>
+                <ul>{listItems}</ul></div>
+            }
+            {Object.keys(user).length !== 0 && <div>
+                <p>Moje prezentacie</p>
+                <ul>{listItems}</ul></div>
+            }
+            {Object.keys(user).length !== 0 && <div>
+                <p>Moje vstupekny</p>
+                <ul>{listItems}</ul></div>
             }
         </div>
     )
