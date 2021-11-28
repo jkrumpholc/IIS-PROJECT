d1c6b2cb0d8e666953062ab7cbfa1b63e0780373
OSBringer <62221766+OSBringer@users.noreply.github.com>
2021-11-28 22:25:38 +0100

    lubosov commit

--- src/components/User.js
+++ src/components/User.js
@@ -1,29 +1,51 @@
-
+import React, {Component, useState , useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import React, { useState , useEffect } from 'react';
 import axios from "axios";
 
-export const User = (props) => {
 
-    const numbers = [1, 2, 3, 4, 5];
-    const listItems = numbers.map((number,index) =>
+export const User = (props) => {
+  const [Konf, setKonf] = useState([]);
+  const [pres, setPres] = useState([]);
+  const [tick, setTick] = useState([]);
+  const numbers = [1, 2, 3, 4, 5];
+  /*const listItems = numbers.map((number,index) =>
 
     <Link to="/clicked_ticket"><li key={index} >{number}</li></Link>
-    );
+    );*/
+
+  const listItems1 = Object.values(Konf).map((item) =>
+    <li>id: {item.id} | popis: {item.description} | žánr: {item.genre} | od: {item.begin_time} | do: {item.end_time} | prodané vstupenky: {item.participants}/{item.capacity}</li>
+  );
+  const listItems2 = Object.values(pres).map((item) =>
+    <li>id: {item.id} | názov prezentácie: {item.name} | konference: {item.conference_name} | od: {item.begin_time} | do: {item.end_time} | potvrzeno: {item.confirmed ? "Ano":"Ne"}</li>
+  );
+  const listItems3 = Object.values(tick).map((item) =>
+    <li>id: {item.id} | konference: {item.conference} | cena: {item.price} | stav: {item.status}</li>
+  );
+        
     
+  console.log({id:props.user['id']});
+
     useEffect(() => {
-        
-
-        
-
-        return () => {
-            
+       console.log(Object.keys(props.user).length!==0);
+       
+       if (Object.keys(props.user).length!==0){
+        axios.post('/profile', {
+            id:props.user['id']
+          })
+          .then(function (response) {
+            //console.log(response.data['tickets']);
+            setKonf(response.data['conferencies'])
+            setPres(response.data['prezentations'])
+            setTick(response.data['tickets'])
+            //console.log(Konf)
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
         }
-    }, [])
-        
-
-
-
+      },[props.user])
+     
     return (
         <div>
             {props.user["result"]==="Success"&&
@@ -31,15 +53,15 @@ export const User = (props) => {
             }
             {props.user["result"]==="Success"&& <div>
                 <p>Moje konferencie</p>
-                <ul>{listItems}</ul></div>
+                <ul>{listItems1}</ul></div>
             }
             {props.user["result"]==="Success"&& <div>
                 <p>Moje prezentacie</p>
-                <ul>{listItems}</ul></div>
+                <ul>{listItems2}</ul></div>
             }
             {props.user["result"]==="Success" && <div>
                 <p>Moje vstupekny</p>
-                <ul>{listItems}</ul></div>
+                <ul>{listItems3}</ul></div>
             }
         </div>
     )
